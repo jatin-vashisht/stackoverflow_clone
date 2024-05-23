f17a479c92da7953aaa888a944c2974ada5b75aa
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-05-23 19:25:38 +0530

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,46 @@
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-spaceGrotesk",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Stack Overflow - Where Developers Learn, Share, & Build Careers",
+  description:
+    "Stack Overflow is the largest, most trusted online community for developers to learn, share their programming knowledge, and build their careers.",
+  icons: {
+    icon: "/assets/images/site-logo.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        }
+      }}
+    >
       <html lang="en">
-        <body>
-          {/* <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn> */}
-          {children}
-        </body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
