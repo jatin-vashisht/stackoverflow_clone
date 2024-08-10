45e30182710e83705f62bec431b92fbec742f1a0
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-08-10 12:09:13 +0530

    fix tags alignment and background

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -40,10 +40,10 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
           />
       </div>
 
-      <section className="mt-12 flex flex-wrap gap-4">
+      <section className="mt-12 flex flex-wrap gap-3">
         {result.tags.length > 0 ? (
           result.tags.map((tag)=> (
-            <Link href={`/tags/${tag._id}`} key={tag._id} className="shadow-light100_darknone">
+            <Link href={`/tags/${tag._id}`} key={tag._id}>
               <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
                 <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
                   <p className="paragraph-semibold text-dark300_light900">
